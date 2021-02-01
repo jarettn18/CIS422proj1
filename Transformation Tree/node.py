b5bb77076eacec817cc1af0bb09a1b935bd09f93
Zeke Petersen <ezekielp@uoregon.edu>
2021-02-01 12:55:29 -0800

    Made edits to node.py

--- Transformation Tree/node.py
+++ Transformation Tree/node.py
@@ -8,26 +8,110 @@ Version 0.1.0
 Node Class and node implementation
 """
 
+# ------- Begin Node Classes --------------
+
+"""
+Notes from ZP on 2/1/2021
+- We may want to also be passing the list of children (represented by their
+operators) to the super() calls and the init signatures of the child classes
+- I did not yet have time to check to make sure all the attributes set in each
+init function is complete
+- There are a handful of standalone functions that should probably be in the
+tree file
+
+- My edits for now are merely trying to represent the inheritance I had in mind
+"""
+
 # Represents a node of an n-ary tree using a node Class
 class Node:
-
     # creating a new tree node
-    def __init__(self ,key):
-        self.key = key
+    def __init__(self, op, parent_op):
+        self.op = op
+        self.parent_op = parent_op
         self.child = []
-        self.folder = []
+        self.folder = []    # May remove in favor of global tables of function names for each class
+
+    def execute():
+        pass
+
+class prepNode(Node):
+
+    def __init__(self, op, parent_op, ts, starting_date, final_date, increment):
+        super().__init__(op, parent_op)
+        self.ts = ts
+        self.starting_date = starting_date
+        self.final_date = final_date
+        self.increment = increment
+
+    def execute():
+        pass
+        """
+        Use this function when the tree executes
+        """
+
+class modelNode(Node):
+
+    def __init__(self, op, parent_op, x_train, y_train, input_dimension, output_dimension, hidden_layers):
+        super().__init__(op, parent_op)
+        self.x_train = x_train
+        self.y_train = y_train
+        self.input_dimension = input_dimension
+        self.output_dimension = output_dimension
+        self.hidden_layers = hidden_layers
+
+    def execute():
+        pass
+    """
+    Function will execute the tree
+    """
+
+class splitNode(Node):
+
+    def __init__(self, op, parent_op, ts, perc_train, prec_valid, perc_test, output_file):
+        super().__init__(op, parent_op)
+        self.ts = ts
+        self.perc_train = perc_train
+        self.prec_valid = prec_valid
+        #Incomplete
+
+    def execute():
+        pass
+    """
+    Function will execute the tree
+    """
 
 
-# Prints the n-ary tree level wise
-"""
-def find_function(operation):
-    #if operation == "some key word"
-    #    call the correlating function
-    #    make sure the altered data replaces the original data
-    #    for the next "step"
-    print("------")
-"""
+class visualizeNode(Node):
 
+    def __init__(self, op, parent_op, ts):
+        super().__init__(op, parent_op)
+        self.ts = ts
+
+    def execute():
+        pass
+    """
+    Function will execute the tree using the DataFram object ts
+    """
+
+class evalNode(Node):
+
+    def __init__(self, op, parent_op, y_test, y_forecast):
+        super().__init__(op, parent_op)
+        self.y_test = y_test
+        self.y_forecast = y_forecast
+
+
+    def execute():
+        pass
+    """
+    This function will execute the tree
+    """
+
+# ------- End Node Classes --------------
+
+
+
+# Probably should be added as a tree method, not a standalone function -- ZP
 def add_node(self, parent, kid):
     """
     Function that will add the kid node as a child to the parent as an addition to the tree
@@ -42,6 +126,7 @@ def add_node(self, parent, kid):
         print("Cannot add a child node to a parent that doesn't exist")
         return None
 
+# Not entirely sure how this works, but leaving for now -- ZP
 def add_root(rootnode):
     """
     Function will take a string and create a root node for the tree
@@ -50,7 +135,10 @@ def add_root(rootnode):
     """
     root = Node(rootnode)
     return root
-    
+
+# Should probably be a tree method with more checks on the operator
+# Instead of folder, we should probably define external tables to do checks
+# (assuming that is what the folder was for) -- ZP
 def replace(self, old, new):
     """
     Function to replace a process step with a different operator, such as
@@ -67,64 +155,12 @@ def replace(self, old, new):
         self.folder.remove(old)
         return None
 
-class prepNode:
-
-    def __init__(self ,ts, starting_date, final_date, increment):
-        self.ts = ts
-        self.starting_date = starting_date
-        self.final_date = final_date
-        self.increment = increment
-
-def execute():
-    """
-    Use this function when the tree executes
-    """
-    
-class modelNode:
-
-    def __init__(self, x_train, y_train, input_dimension, output_dimension, hidden_layers):
-        self.x_train = x_train
-        self.y_train = y_train
-        self.input_dimension = input_dimension
-        self.output_dimension = output_dimension
-        self.hidden_layers = hidden_layers
-
-def execute_tree():
-    """
-    Function will execute the tree
-    """
-
-class splitNode:
-
-    def __init__(self, ts, perc_train, prec_valid, perc_test, output_file):
-        self.ts = ts
-        self.perc_train = perc_train
-        self.prec_valid = prec_valid
-
-def execute_tree():
-    """
-    Function will execute the tree
-    """
-
-
-class visualizeNode:
-
-    def __init__(self, ts):
-        self.ts = ts
-
-def execute_tree():
-    """
-    Function will execute the tree using the DataFram object ts
-    """
-    
-class evalNode:
-
-    def __init__(self, y_test, y_forecast):
-        self.y_test = y_test
-        self.y_forecast = y_forecast
-
-
-def execute_tree():
-    """
-    This function will execute the tree
-    """
+# Prints the n-ary tree level wise
+"""
+def find_function(operation):
+    #if operation == "some op word"
+    #    call the correlating function
+    #    make sure the altered data replaces the original data
+    #    for the next "step"
+    print("------")
+"""
