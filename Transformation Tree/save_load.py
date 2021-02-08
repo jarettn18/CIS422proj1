8891ec621572bafeb70c3e3e83aad09e02377594
westcallista <77295181+westcallista@users.noreply.github.com>
2021-02-07 17:23:17 -0800

    Update save_load.py
    removed Save class, load_tree() returns tree object now

--- Transformation Tree/save_load.py
+++ Transformation Tree/save_load.py
@@ -11,41 +11,35 @@ Saving and Loading a Tree
 import pickle
 from pathlib import Path
 
-"""
-class Save used to save a tree as well as load a tree
-"""
+def save_tree(tree, file_name):
+    """
+    :tree: a tree object wanting to be saved
+    :file_name: name of file wanting tree object to be saved to (str)
 
-class Save:
-    def __init__(self):
-        pass
+    Function uses pickle to open file_name with write access to write object to a file
+    Function saves the file into directory pickle_objects
+    Function returns None
+    """
+    root = Path(".")
+    my_path = root / "pickle_objects" / file_name
+    pickle_out = open(my_path, "wb")
+    pickle.dump(tree, pickle_out)
+    pickle_out.close()
+    return None
 
-    def save_tree(tree, file_name):
-        """
-        :tree: a tree object wanting to be saved
-        :file_name: name of file wanting tree object to be saved to (str)
+def load_tree(tree, file_name):
+    """
+    : tree: same tree object that was passed into save_tree()
+    : file_name: same file name (file_name) that the tree object was saved to in save_tree()
 
-        Function uses pickle to open file_name with write access to write object to a file
-        Function saves the file into directory pickle_objects
-        Function returns None
-        """
-        root = Path(".")
-        my_path = root / "pickle_objects" / file_name
-        pickle_out = open(my_path, "wb")
-        pickle.dump(tree, pickle_out)
-        pickle_out.close()
-        return None
-
-    def load_tree(tree, file_name):
-        """
-        : tree: same tree object that was passed into save_tree()
-        : file_name: same file name (file_name) that the tree object was saved to in save_tree()
-
-        Function opwns the file_name with read access and uses pickle.load() to load the file
-          to a var.
-        Function returns None
-        """
-        root = Path(".")
-        my_path = root / "pickle_objects" / file_name
-        pickle_in = open(my_path, "rb")
-        tree = pickle.load(pickle_in)
-        return None
+    Function opens the file_name with read access and uses pickle.load() to load the file
+        to a var.
+    The file is saved within the directory "pickle_objects", which is stored within the
+        Transformation Tree directory
+    Function returns the tree object that had been saved previously by the save_tree() function
+    """
+    root = Path(".")
+    my_path = root / "pickle_objects" / file_name
+    pickle_in = open(my_path, "rb")
+    tree = pickle.load(pickle_in)
+    return tree
